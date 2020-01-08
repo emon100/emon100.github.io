9d479cc3f99f4eee55585fcb90985bc64826a0dc
emon100 <emon100@qq.com>
2020-01-08 15:17:47 +0800

    Turing machine can add transition functions now.

--- general-turing-machine/js/tm.js
+++ general-turing-machine/js/tm.js
@@ -26,13 +26,28 @@ let app = {
     },
 
     moves : [],
-    preset1(){
+    setPreset1(){
+        this.headInit();
+        this.ruleTable.initial = "q0";
+        this.ruleTable.final = "q2";
         this.tape=['B','0','0','1','1','0','0','1','1','0','B'];
+
+        var a = new Map();
+        this.ruleTable.states.set("q0",a);
+        this.ruleTable.states.get('q0').set('0',{nextState:"q0",write:"1",move:1});
+        this.ruleTable.states.get("q0").set('1',{nextState:"q0",write:"0",move:1});
+        this.ruleTable.states.get("q0").set('B',{nextState:"q1",write:"B",move:-1});
+        var b = new Map([['0',{nextState:"q1",write:"w",move:-1}]]);
+        this.ruleTable.states.set("q1",b);
+        this.ruleTable.states.get("q1").set('1',{nextState:"q1",write:"w",move:-1});
+        this.ruleTable.states.get("q1").set('B',{nextState:"q2",write:"B",move:1});
+        this.updateView();
+        },
+    setPreset2(){
+        this.headInit();
         this.ruleTable.initial = "q0";
         this.ruleTable.final = "q2";
-        this.head.index=1;
-        this.head.currentState=this.ruleTable.initial;
-
+        this.tape=['B','0','0','1','1','0','0','1','1','0','B'];
 
         var a = new Map();
         this.ruleTable.states.set("q0",a);
@@ -44,57 +59,47 @@ let app = {
         this.ruleTable.states.get("q1").set('1',{nextState:"q1",write:"w",move:-1});
         this.ruleTable.states.get("q1").set('B',{nextState:"q2",write:"B",move:1});
     },
-    preset2(){
-        this.ruleTable.initial = "q0";
-        this.ruleTable.final = "q2";
+    headInit(){
         this.head.index=1;
         this.head.currentState=this.ruleTable.initial;
-
-
-        var a = new Map();
-        this.ruleTable.states.set("q0",a);
-        this.ruleTable.states.get('q0').set('0',{nextState:"q0",write:"1",move:1});
-        this.ruleTable.states.get("q0").set('1',{nextState:"q0",write:"0",move:1});
-        this.ruleTable.states.get("q0").set('B',{nextState:"q1",write:"B",move:-1});
-        var b = new Map([['0',{nextState:"q1",write:"w",move:-1}]]);
-        this.ruleTable.states.set("q1",b);
-        this.ruleTable.states.get("q1").set('1',{nextState:"q1",write:"w",move:-1});
-        this.ruleTable.states.get("q1").set('B',{nextState:"q2",write:"B",move:1});
-    },
+  },
     start() {
         this.moves=[];
-        this.preset1();
-
         this.updateView();
     },
-    updateStatesView: function () {
-        $("#states-list").empty();
-        let ol2 = document.createElement("ol");
-        ol2.innerText="初始状态"+this.ruleTable.initial+"\n结束状态"+this.ruleTable.final;
-        $("#states-list").append(ol2);
-        this.ruleTable.states.forEach((v,k) => {
-            var f = function (value, key,map){
-                let ol = document.createElement("ol");
-                ol.innerText = "δ("+k+" , "+key+") => δ("+value.nextState+" , "+value.write+" , "+value.move+")";
-                $("#states-list").append(ol);
-            };
-            v.forEach(f);
-        });
-    },
-
     updateView(){
         this.updateStatesView();
         this.displayTape();
         this.displayHead();
     },
+    updateStatesView: function () {
+        $("#start").val(this.ruleTable.initial);
+        $("#end").val(this.ruleTable.final);
+
+        var statesList = $("#states-list");
+        states-list.empty();
+        let ol2 = document.createElement("ol");
+        ol2.innerText="初始状态"+(this.ruleTable.initial==null?"为空":this.ruleTable.initial)+"\n结束状态"+(this.ruleTable.final==null?"为空":this.ruleTable.final);
+        statesList.append(ol2);
+        this.ruleTable.states.forEach((v,k) => {
+            var f = function (value, key){
+                let ol = document.createElement("ol");
+                ol.innerText = "δ("+k+" , "+key+") => δ("+value.nextState+" , "+value.write+" , "+(value.move === 1?'R':value.move === -1?'L':'S')+")";
+                statesList.append(ol);
+            };
+            v.forEach(f);
+        });
+    },
+
     displayHead(){//viewer刷新读写头
-        $(".row2").empty();
+        var row2 =$(".row2");
+            row2.empty();
         this.tape.forEach(function() {
             let col2 = document.createElement("th");
             col2.innerText;
             $('.row2').append(col2);
         });
-        $(".row2").children()[this.head.index].innerText='▲';//设置箭头
+        row2.children()[this.head.index].innerText='▲';//设置箭头
     },
     displayTape() {//viewer刷新纸带
         var tape=this.tape;
@@ -106,38 +111,40 @@ let app = {
         });
     },
     putInputOnTape(){//controller
+        this.headInit();
         this.clearTape();
         this.tape.pop();
         this.tape = this.tape.concat($('#add').val().split(''));
         this.tape.push('B');
-        this.updateView();
     },
     clearTape(){
         this.tape = ['B','B'];
     },
-    addTransitionFunction(){
-        alert("Haven't done yet")
-    },
-    parse(){
+    changeInitFinal(){
         var initial = $("#start").val();
-        var final = $("#end").val;
+        var final = $("#end").val();
+        this.ruleTable.initial=initial;
+        this.ruleTable.final=final;
+        this.updateStatesView();
+    },
+    changeTransitionFuncitons(){
+
         var currentstate = $(".current-state").val();
         var tapeSymbol = $(".tape-symbol").val();
         var nextstate = $(".next-state").val();
         var writeSymbol = $(".write-symbol").val();
-        var move = $(".move").val()=='L'?-1:$(".move").val()=='R'?1:0;
+        var move = $(".move").val()==='L'?-1:$(".move").val()==='R'?1:0;
 
         var map = new Map();
-        if(this.ruleTable.states.get(currentstate)==undefined){
+        if(this.ruleTable.states.get(currentstate)===undefined){
             this.ruleTable.states.set(currentstate,map);
         }
         this.ruleTable.states.get(currentstate).set(tapeSymbol,{nextState:nextstate,write:writeSymbol,move:move});
 
         this.updateStatesView();
-
     },
     oneStepBackward() {//回退一步
-        if(this.moves.length!=0){
+        if(this.moves.length!==0){
             var M =this.moves.pop();
             this.head.index=M.index;
             this.head.currentState=M.state;
@@ -148,7 +155,7 @@ let app = {
         }
     },
     oneStepForward(){//进行一步
-        if(this.head.currentState==this.ruleTable.final){//终止
+        if(this.head.currentState===this.ruleTable.final){//终止
             $('#modal-container-1').modal('show');
             return;
         }
@@ -161,12 +168,12 @@ let app = {
         var currentState = this.head.currentState;
         var rule = this.ruleTable.states.get(currentState);
 
-        if(rule == undefined) {//无此状态
+        if(rule === undefined) {//无此状态
             $('#modal-container-2').modal('show');
             return;
         }else {//此状态无读到此字符的操作
             rule = rule.get(currentSymbol);
-            if(rule == undefined){
+            if(rule === undefined){
                 $('#modal-container-2').modal('show');
                 return;
             }
@@ -175,7 +182,7 @@ let app = {
             this.head.currentState = rule.nextState;//设置状态
             this.tape[this.head.index]=rule.write;//写入纸带
             this.head.index += rule.move;//移动读头
-            if(this.head.index==this.tape.length){this.tape.push('B');}//向右延伸
+            if(this.head.index===this.tape.length){this.tape.push('B');}//向右延伸
             if(this.head.index<0){
                 $('#modal-container-3').modal('show');
                 return;
@@ -187,8 +194,9 @@ let app = {
 
 $(function () {//保证DOM加载再开始程序
     $('#submit').click(function () {
-        app.start();
         app.putInputOnTape();
+        app.headInit();
+        app.updateView();
     });
     $('#onstepback').click(function () {
         app.oneStepBackward();
@@ -196,9 +204,15 @@ $(function () {//保证DOM加载再开始程序
     $('#onstepforward').click(function () {
         app.oneStepForward();
     });
+    $('#change-initial-final').click(function () {
+        app.changeInitFinal();
+    });
     $('#addTransitionFunction').click(function () {
-        app.parse();
-    })
+        app.changeTransitionFuncitons();
+    });
+    $('#preset1').click(function () {
+        app.setPreset1();
+    });
     app.start();
 });
 
